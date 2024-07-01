bf3c0cba0bedf52ddf9589e43479fd84edc26c0d
Timi <ksngk@gmail.com>
2024-07-01 20:41:10 +0200

    Add cars

--- src/main/java/com/cars/carsspring/service/CarController.java
+++ src/main/java/com/cars/carsspring/service/CarController.java
@@ -1,2 +1,27 @@
-package com.cars.carsspring.service;public class CarController {
+package com.cars.carsspring.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/cars")
+public class CarController {
+    private List<Car> cars = new ArrayList<>();
+    @GetMapping
+    public List<Car> getAllCars(){
+
+        return null;
+    }
+
+    @GetMapping
+    public List<Car> getCarById(@PathVariable Long id){
+        return null;
+    }
+
 }
